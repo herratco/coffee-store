c2234a81b7d0dd1dd7d2e1221f520c100afa1f0c
herratco <herratco@yahoo.com>
2023-09-05 08:24:02 -0500

    version

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,11 +1,17 @@
-
+import {version} from "react"
+import appVersion from "../../package.json"
+import PropTypes from "prop-types"
 
 const ItemListContainer = ({greeting}) => {
     return (
         <div>
             <h2>{greeting}</h2>
+            <h2>React version: {version}</h2>
+            <h2>This App version: {appVersion.version}</h2>
         </div>
     )
 }
-
+ItemListContainer.propTypes = {
+    greeting: PropTypes.string
+  };
 export default ItemListContainer
